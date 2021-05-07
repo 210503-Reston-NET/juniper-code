cb838286d58c28af754bd3e35b561998e4ad410c
Juniper Song <songminseonk@gmail.com>
2021-05-06 20:37:52 -0700

    XML FILE WRITINGgit add .

--- 1-csharp/RestaurantReviews/RRDL/RepoFile.cs
+++ 1-csharp/RestaurantReviews/RRDL/RepoFile.cs
@@ -2,21 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
 using RRModels;
 
 namespace RRDL
 {
     public class RepoFile : IRepository
     {
-        private const string filePath = "../RRDL/Restaurants.json";
-        private string jsonString;
+        private const string filePath = "../RRDL/Restaurants.xml";
+        // private string jsonString;
+        private string xmlString;
         public Restaurant AddRestaurant(Restaurant restaurant)
         {
-            List<Restaurant> restauransFromFile = GetAllRestaurants();
-            restauransFromFile.Add(restaurant);
-            jsonString = JsonSerializer.Serialize(restauransFromFile);
-            File.WriteAllText(filePath, jsonString);
+            List<Restaurant> restaurantsFromFile = GetAllRestaurants();
+            restaurantsFromFile.Add(restaurant);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Restaurant>));
+            using (Stream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                XmlWriter writer = new XmlTextWriter(fs, Encoding.Unicode);
+                serializer.Serialize(writer, restaurantsFromFile);
+                writer.Close();
+            }
+            // jsonString = JsonSerializer.Serialize(restaurantsFromFile);
+            // File.WriteAllText(filePath, jsonString);
+
             return restaurant;
         }
 
@@ -24,14 +36,19 @@ namespace RRDL
         {
             try
             {
-                jsonString = File.ReadAllText(filePath);
+                xmlString = File.ReadAllText(filePath);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return new List<Restaurant>();
             }
-            return JsonSerializer.Deserialize<List<Restaurant>>(jsonString);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Restaurant>));
+            using (Stream reader = new FileStream(filePath, FileMode.Open))
+            {
+                return (List<Restaurant>)serializer.Deserialize(reader);
+            }
+            // return JsonSerializer.Deserialize<List<Restaurant>>(jsonString);
         }
 
         public Restaurant GetRestaurant(Restaurant restaurant)
